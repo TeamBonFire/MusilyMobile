363208610dadfc2b115d07270b590caa4af83c55
okt <andyoh12@naver.com>
2020-10-31 16:01:02 +0900

    redux설치, 앱구조 작성

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="PlayList" /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/containers/Home/slice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface HomeState {
+  group_cnt: number;
+}
+
+const initialState: HomeState = {
+  group_cnt: 0,
+};
+
+const slice = createSlice({
+  name: 'home',
+  initialState,
+  reducers: {
+    addGroup: (state) => {
+      state.group_cnt + 1;
+    },
+  },
+});
+
+export const { addGroup } = slice.actions;
+export default slice.reducer;
