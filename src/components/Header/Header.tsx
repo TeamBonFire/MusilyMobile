bd807495ddb6a9345236233f03c4433a38e90f78
okt <andyoh12@naver.com>
2020-11-06 01:52:26 +0900

    Header 컴포넌트 추가

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Hamburger from './Hamburger';
+
+const Header = ({}) => (
+  <View style={styles.header}>
+    <Hamburger />
+    <View style={styles.profile_icon} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 19,
+    paddingRight: 19,
+    width: '100%',
+    height: 65,
+  },
+  profile_icon: {
+    width: 41,
+    height: 41,
+    borderRadius: 20,
+    backgroundColor: '#C4C4C4',
+  },
+});
+
+export default Header;
